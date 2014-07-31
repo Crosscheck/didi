654f6af2483168aa9b77c6413aa3cc984b4781e6
LeShadow <sebastiaan.provost@student.uantwerpen.be>
2014-07-31 14:40:33 +0200

    test

--- lib/didi/recipes/didi.rb
+++ lib/didi/recipes/didi.rb
@@ -61,11 +61,18 @@ _cset :drush_path,        ''
 #_cset(:shared_private_files)    { domain.to_a.map { |d| File.join(shared_path, d, private_files) } }
 #_cset(:dbbackups_path)          { domain.to_a.map { |d| File.join(deploy_to, dbbackups, d) } }
 #_cset(:drush)                   { "drush -r #{current_path}" + (domain == 'default' ? '' : " -l #{domain}") }  # FIXME: not in use?
-
-_cset(:shared_settings)         { domain.split(' ').map { |d| File.join(shared_path, d, settings) } }
-_cset(:shared_files)            { domain.split(' ').map { |d| File.join(shared_path, d, files) } }
-_cset(:shared_private_files)    { domain.split(' ').map { |d| File.join(shared_path, d, private_files) } }
-_cset(:dbbackups_path)          { domain.split(' ').map { |d| File.join(deploy_to, dbbackups, d) } }
+if baseline.kind_of?(String)
+    baseline.lines.to_a
+end
+    
+if domain.kind_of?(String)
+    domain.lines.to_a
+end
+    
+_cset(:shared_settings)         { domain.map { |d| File.join(shared_path, d, settings) } }
+_cset(:shared_files)            { domain.map { |d| File.join(shared_path, d, files) } }
+_cset(:shared_private_files)    { domain.map { |d| File.join(shared_path, d, private_files) } }
+_cset(:dbbackups_path)          { domain.map { |d| File.join(deploy_to, dbbackups, d) } }
 _cset(:drush)                   { "drush -r #{current_path}" + (domain == 'default' ? '' : " -l #{domain}") }  # FIXME: not in use?
     
     
@@ -94,15 +101,15 @@ set(:previous_revision) { capture("cat #{previous_release}/REVISION", :except =>
 
 #_cset(:is_multisite)                  { domain.to_a.size > 1 }
 
-_cset(:release_settings)              { domain.split(' ').map { |d| File.join(release_path, drupal_path, 'sites', d, settings) } }
-_cset(:release_files)                 { domain.split(' ').map { |d| File.join(release_path, drupal_path, 'sites', d, files) } }
-_cset(:release_private_files)         { domain.split(' ').map { |d| File.join(release_path, drupal_path, 'sites', d, private_files) } }
-_cset(:release_domain)                { domain.split(' ').map { |d| File.join(release_path, drupal_path, 'sites', d) } }
+_cset(:release_settings)              { domain.map { |d| File.join(release_path, drupal_path, 'sites', d, settings) } }
+_cset(:release_files)                 { domain.map { |d| File.join(release_path, drupal_path, 'sites', d, files) } }
+_cset(:release_private_files)         { domain.map { |d| File.join(release_path, drupal_path, 'sites', d, private_files) } }
+_cset(:release_domain)                { domain.map { |d| File.join(release_path, drupal_path, 'sites', d) } }
 
-_cset(:previous_release_settings)             { releases.length > 1 ? domain.split(' ').map { |d| File.join(previous_release, drupal_path, 'sites', d, settings) } : nil }
-_cset(:previous_release_files)                { releases.length > 1 ? domain.split(' ').map { |d| File.join(previous_release, drupal_path, 'sites', d, files) } : nil }
-_cset(:previous_release_private_files)        { releases.length > 1 ? domain.split(' ').map { |d| File.join(previous_release, drupal_path, 'sites', d, private_files) } : nil }
-_cset(:previous_release_domain)               { releases.length > 1 ? domain.split(' ').map { |d| File.join(previous_release, drupal_path, 'sites', d) } : nil }
+_cset(:previous_release_settings)             { releases.length > 1 ? domain.map { |d| File.join(previous_release, drupal_path, 'sites', d, settings) } : nil }
+_cset(:previous_release_files)                { releases.length > 1 ? domain.map { |d| File.join(previous_release, drupal_path, 'sites', d, files) } : nil }
+_cset(:previous_release_private_files)        { releases.length > 1 ? domain.map { |d| File.join(previous_release, drupal_path, 'sites', d, private_files) } : nil }
+_cset(:previous_release_domain)               { releases.length > 1 ? domain.map { |d| File.join(previous_release, drupal_path, 'sites', d) } : nil }
 
 _cset(:is_multisite)                  { domain.split(' ').size > 1 }
     
@@ -113,12 +120,7 @@ _cset(:is_multisite)                  { domain.split(' ').size > 1 }
 depend :local,  :command, "drush"
 depend :remote, :command, "#{drush_path}drush"
 
-#if !domain.empty? && !domain.kind_of?(Array)
-#    domain = [domain]
-#end
-#if !baseline.empty? && !baseline.kind_of?(Array)
-#    baseline = [baseline]
-#end
+
 # =========================================================================
 # Overwrites to the DEPLOY tasks in the capistrano library.
 # =========================================================================
@@ -385,7 +387,7 @@ namespace :drush do
   task :bl do
     domain.each do |d|
       #baseline.to_a.each do |bl_item|
-      baseline.split(' ').each do |bl_item|
+      baseline.each do |bl_item|
         run "cd #{current_path}/#{drupal_path} && #{drush_path}drush" + (d == 'default' ? '' : " -l #{d}") + " pm-enable #{bl_item.gsub("%domain", d)} -y"
       end
     end
