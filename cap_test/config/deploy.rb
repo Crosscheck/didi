d896a27c82df0a79014d8c85450801dea76aa838
Klaas Van Waesberghe <klaasvw@gmail.com>
2014-12-22 12:49:47 +0100

    Issue #PE-75: Remove test files.

--- cap_test/config/deploy.rb
+++
@@ -1,5 +0,0 @@
-set :default_stage, "staging"
-set :stages, %w(staging production)
-set :application, "test"
-set :repository,  "test@gitlab.crosscheck.be"
-set :drupal_path, "docroot" # change this to your drupal folder
